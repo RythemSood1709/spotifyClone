c7186d36f8347e50d9e718904792befdc34c9321
Rythem Sood <rythemsoodlm10@gmail.com>
2024-11-17 03:05:16 +0530

    Added Songlist

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
     }
 
@@ -25,9 +25,28 @@ async function main(){
     let songs=await getSongs();
     console.log(songs);
 
-    //play first song
-    // var audio=new Audio(songs[0]);
-    // audio.play();
-}
+    let songUL=document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for(const song of songs){
+        songUL.innerHTML=songUL.innerHTML+`<li>
+                        <img class="invert" src="svg/music.svg" alt="">
+                        <div class="info">
+                            <div>${song.replaceAll("%20"," ").replace(".mp3","")}</div>
+                            <div>Song Artist</div>
+                        </div>
+                        <div class="playNow">
+                        <span>Play Now</span>
+                        <img class="invert" src="svg/play.svg" alt="">
+                        </div>
+                        </li>`
+    }
 
-main()
+    //play first song
+    var audio=new Audio(songs[1]);
+    audio.play();  
+
+    audio.addEventListener("loadeddata",()=> {
+        console.log(audio.duration,audio.currentSrc,audio.currentTime);
+    })   
+}        
+ 
+main()    
