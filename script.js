06f5db2870a99056d94a4315eba1672bee56c08b
Rythem Sood <rythemsoodlm10@gmail.com>
2024-11-19 00:42:19 +0530

    Error Fixed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("JS");
 let currentSong=new Audio();
 // returns songs
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let a = await fetch("http://127.0.0.1:5500/Assets/songs/")
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -22,7 +22,7 @@ async function getSongs() {
 
 const playMusic= (track)=> {
     // let audio=new Audio("/songs/"+track)
-    currentSong.src="/songs/"+track
+    currentSong.src="Assets/songs/"+track
     currentSong.play()
 }
 
@@ -35,14 +35,14 @@ async function main() {
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li>
-                        <img class="invert" src="svg/music.svg" alt="">
+                        <img class="invert" src="Assets/svg/music.svg" alt="">
                         <div class="info">
                             <div>${song.replaceAll("%20", " ").replace(".mp3", "")}</div>
                             <div>Song Artist</div>
                         </div>
                         <div class="playNow">
                         <span>Play Now</span>
-                        <img class="invert" src="svg/play.svg" alt="">
+                        <img class="invert" src="Assets/svg/play.svg" alt="">
                         </div> </li>`;
     }
 
